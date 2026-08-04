dc037fd04a4db099483dfdfd456ea1898e4d1ff9
Achal Sunil Padol <achalpadol@gmail.com>
2026-08-04 13:19:17 +0530

    Add files via upload

---
+++ TERRAFORM/THREE-TEIR-ARCHI/Module/SG/main.tf
@@ -0,0 +1,71 @@
+resource "aws_security_group" "SG" {
+  count = length(var.sg_name)
+  name        = var.sg_name[count.index]
+  description = "Security Group for ALB"
+  vpc_id      = var.vpc_id
+  tags = {
+    Name = var.sg_name[count.index]
+  }
+}
+
+# alb ingress
+resource "aws_security_group_rule" "alb_ingress" {
+  count = length(var.alb_ports)
+  type = "ingress"
+  from_port = var.alb_ports[count.index]
+  to_port   = var.alb_ports[count.index]
+  protocol = var.ingress_protocol
+  cidr_blocks = var.alb_ingress_cidr
+  security_group_id = aws_security_group.SG[0].id
+}
+
+# alb egress
+resource "aws_security_group_rule" "alb_egress" {
+  type = "egress"
+  from_port = var.egress_from_port
+  to_port = var.egress_to_port
+  protocol = var.egress_protocol
+  cidr_blocks = var.egress_cidr
+  security_group_id = aws_security_group.SG[0].id
+}
+
+# application ingress
+resource "aws_security_group_rule" "app_ingress" {
+  count = length(var.app_ports)
+  type = "ingress"
+  from_port = var.app_ports[count.index]
+  to_port   = var.app_ports[count.index]
+  protocol = var.ingress_protocol
+  source_security_group_id = aws_security_group.SG[0].id
+  security_group_id = aws_security_group.SG[1].id
+}
+# Egress application
+resource "aws_security_group_rule" "app_egress" {
+  type = "egress"
+  from_port = var.egress_from_port
+  to_port = var.egress_to_port
+  protocol = var.egress_protocol
+  cidr_blocks = var.egress_cidr
+  security_group_id = aws_security_group.SG[1].id
+}
+
+# DB ingress
+resource "aws_security_group_rule" "db_ingress" {
+  count = length(var.db_ports)
+  type = "ingress"
+  from_port = var.db_ports[count.index]
+  to_port   = var.db_ports[count.index]
+  protocol = var.ingress_protocol
+  source_security_group_id = aws_security_group.SG[1].id
+  security_group_id = aws_security_group.SG[2].id
+}
+
+# egress db
+resource "aws_security_group_rule" "db_egress" {
+  type = "egress"
+  from_port = var.egress_from_port
+  to_port = var.egress_to_port
+  protocol = var.egress_protocol
+  cidr_blocks = var.egress_cidr
+  security_group_id = aws_security_group.SG[2].id
+}
---
+++ TERRAFORM/THREE-TEIR-ARCHI/Module/SG/variable.tf
@@ -0,0 +1,53 @@
+variable "vpc_id" {
+  description = "VPC ID where Security Groups will be created"
+  type        = string
+}
+
+variable "sg_name" {
+  description = "List of Security Group Names (ALB, APP, DB)"
+  type        = list(string)
+}
+
+variable "alb_ports" {
+  description = "Ports allowed for ALB"
+  type        = list(number)
+}
+
+variable "app_ports" {
+  description = "Ports allowed for Application"
+  type        = list(number)
+}
+
+variable "db_ports" {
+  description = "Ports allowed for Database"
+  type        = list(number)
+}
+
+variable "alb_ingress_cidr" {
+  description = "CIDR block allowed to access ALB"
+  type = list(string)
+}
+
+variable "egress_cidr" {
+  description = "CIDR block for outbound traffic"
+  type = list(string)
+}
+
+variable "egress_from_port" {
+  description = "Starting outbound port"
+  type = number
+}
+
+variable "egress_to_port" {
+  description = "Ending outbound port"
+  type = number
+}
+
+variable "egress_protocol" {
+  description = "Protocol for outbound traffic"
+  type = string
+}
+variable "ingress_protocol" {
+  description = "Protocol used for ingress rules"
+  type = string
+}
