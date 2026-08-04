b58cb4b2866128705bd53f5c1115735df5ea8c73
Achal Sunil Padol <achalpadol@gmail.com>
2026-08-04 13:12:59 +0530

    Update variable.tf

--- TERRAFORM/THREE-TEIR-ARCHI/Module/VPC/variable.tf
+++ TERRAFORM/THREE-TEIR-ARCHI/Module/VPC/variable.tf
@@ -5,6 +5,27 @@ variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
 }
+variable "region_name" {
+  description = "region name"
+  type = string
+}
+variable "enable_dns_support" {
+  description = "Enable DNS Support"
+  type = bool
+}
+
+variable "enable_dns_hostnames" {
+  description = "Enable DNS Hostnames"
+  type = bool
+}
+
+variable "vpc_name" {
+  description = "VPC Name"
+  type = string
+}
+variable "igw_name"{
+  type = string
+}
 
 # public subnet
 variable "public_subnet_cidrs" {
@@ -20,6 +41,10 @@ variable "public_subnet_azs" {
 variable "public_subnet_names" {
   description = "List of names for Public Subnets"
   type        = list(string)
+}
+variable "map_public_ip_on_launch" {
+  description = "Assign public IP automatically"
+  type = bool
 }
 
 # Private subnet
@@ -54,4 +79,28 @@ variable "private_db_subnet_names" {
   description = "List of names for Private Database Subnets"
   type        = list(string)
 }
+variable "my_lastic_ip"{
+  type = "string"
+}
+variable "my_nat_gateway"{
+  type "string"
+}
 
+variable "private_route_cidr" {
+  description = "Destination CIDR for the private route"
+  type = string
+}
+
+variable "private_route_table_name" {
+  description = "Private Route Table Name"
+  type = string
+}
+variable "public_route_cidr" {
+  description = "Destination CIDR for Public Route Table"
+  type = string
+}
+
+variable "public_route_table_name" {
+  description = "Public Route Table Name"
+  type = string
+}
