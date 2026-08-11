a04d369445d7df2cbd599f6edc29de4b70386c5a
Achal Sunil Padol <achalpadol@gmail.com>
2026-08-11 10:21:11 +0530

    Delete TERRAFORM/THREE-TEIR-ARCHI/Module/ECS-CLUSTER directory

--- TERRAFORM/THREE-TEIR-ARCHI/Module/ECS-CLUSTER/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "CLUSTER_NAME" {
-  description = "ECS Cluster Name"
-  type        = string
-}
-variable "CONTAINER_INSIGHTS" {
-  description = "Enable ECS Container Insights"
-  type        = string
-  default     = "enabled"
-}
