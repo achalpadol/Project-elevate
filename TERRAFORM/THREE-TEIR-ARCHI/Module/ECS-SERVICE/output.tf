16a6ef446401fab044ace593c9fdaeb3f503b214
Achal Sunil Padol <achalpadol@gmail.com>
2026-08-11 10:21:29 +0530

    Delete TERRAFORM/THREE-TEIR-ARCHI/Module/ECS-SERVICE directory

--- TERRAFORM/THREE-TEIR-ARCHI/Module/ECS-SERVICE/output.tf
+++
@@ -1,9 +0,0 @@
-output "service_id" {
-  value = aws_ecs_service.this_service.id
-}
-output "service_name" {
-  value = aws_ecs_service.this_service.name
-}
-output "service_arn" {
-  value = aws_ecs_service.this_service.arn
-}
