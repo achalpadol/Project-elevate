efc4e7a1640830136ede3a4837611d32634f6070
Achal Sunil Padol <achalpadol@gmail.com>
2026-08-11 10:33:41 +0530

    Add files via upload

---
+++ TERRAFORM/ECS-DEPLOYMENT/module/IAM/variable.tf
@@ -0,0 +1,15 @@
+variable "IAM_ROLE_NAME" {
+  type = string
+}
+variable "INSTANCE_PROFILE_NAME" {
+  type = string
+}
+variable "SSM_POLICY_ARN" {
+  type = string
+}
+variable "EXECUTION_ROLE_NAME" {
+  type = string
+}
+variable "TASK_ROLE_NAME" {
+  type = string
+}
