eff640984ae8725ce0c1cf7256fc262924827216
Achal Sunil Padol <achalpadol@gmail.com>
2026-08-14 10:11:56 +0530

    Update output.tf

--- TERRAFORM/ECS-DEPLOYMENT/module/IAM/output.tf
+++ TERRAFORM/ECS-DEPLOYMENT/module/IAM/output.tf
@@ -1,24 +1,16 @@
-output "iam_role_name" {
-  value = aws_iam_role.this_ssm_role.name
-}
-output "iam_role_arn" {
-  value = aws_iam_role.this_ssm_role.arn
-}
-output "instance_profile_name" {
-  value = aws_iam_instance_profile.this_ssm_profile.name
-}
-output "instance_profile_arn" {
-  value = aws_iam_instance_profile.this_ssm_profile.arn
-}
 output "execution_role_arn" {
-  value = aws_iam_role.this_ecs_execution_role.arn
-}
-output "task_role_arn" {
-  value = aws_iam_role.this_ecs_task_role.arn
+  description = "ARN of the ECS Task Execution Role"
+  value       = aws_iam_role.this_ecs_execution_role.arn
 }
 output "execution_role_name" {
-  value = aws_iam_role.this_ecs_execution_role.name
+  description = "Name of the ECS Task Execution Role"
+  value       = aws_iam_role.this_ecs_execution_role.name
+}
+output "task_role_arn" {
+  description = "ARN of the ECS Task Role"
+  value       = aws_iam_role.this_ecs_task_role.arn
 }
 output "task_role_name" {
-  value = aws_iam_role.this_ecs_task_role.name
+  description = "Name of the ECS Task Role"
+  value       = aws_iam_role.this_ecs_task_role.name
 }
